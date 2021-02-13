9d0d71bef74611cc2f6a7fb5cc55b05708ca15df
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-02-13 14:30:15 -0300

    deixei tudo dentro do .h por conta dos templates e corrigi o uso deles

--- HashTable.hpp
+++ HashTable.hpp
@@ -16,21 +16,81 @@ class impossivel_criar_tabela_exception : public std::exception
         return "Não foi possivel inicializar a Tabela";
     }
 };
+class adicionar_em_tabela_nula_exception : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Não é possivel adicionar em null";
+    }
+};
+
+class chave_igual_exception : public std::exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Não é possivel adicionar com chaves repetidas";
+    }
+};
 template <typename TKey, typename TData>
 struct HashTable
 {
-    int _tamanho;
-    Elemento<TKey, TData> **_itens;
+    int *_tamanho;
+    ListaEncadeada<TKey, TData> **_itens;
 };
+int stringValue(string str)
+{
+    int val;
+    for (int i = 0; i < str.length(); i++)
+        val = val + i * str[i];
+    return val;
+}
+
+template <typename T>
+int hashFunction(T chave, int tam)
+{
+    int k;
+    if (typeid(chave) == typeid(string))
+        k = stringValue(chave);
+    if (typeid(chave) == typeid(char))
+        k = chave[0];
+    else
+        k = floor(chave[0]);
+    k = k % tam;
+    return k;
+}
 template <typename TKey, typename TData>
-void inicializaHashTable(HashTable<TKey, TData> *tabela, int tamanho);
+void inicializaHashTable(HashTable<TKey, TData> *tabela, int *tamanho)
+{
+    tabela = (HashTable<TKey, TData> *)malloc(sizeof(HashTable<TKey, TData>));
+    if (tabela == nullptr)
+        throw impossivel_criar_tabela_exception();
+    tabela->_tamanho = tamanho;
+    tabela->_itens = (ListaEncadeada<TKey, TData> **)malloc(*tamanho * sizeof(ListaEncadeada<TKey, TData>));
+    if (tabela->_itens == nullptr)
+        throw impossivel_criar_tabela_exception();
+}
 template <typename TKey, typename TData>
-void destroiHashTable(HashTable<TKey, TData> *tabela);
+void adicionarNaHashTable(HashTable<TKey, TData> *tabela, TKey *chave, TData *valor)
+{
+    int pos = hashFunction(*chave, *tabela->_tamanho);
+    if (tabela == nullptr)
+        throw adicionar_em_tabela_nula_exception();
+    adicionaNoFim(tabela->_itens[pos], valor);
+}
 template <typename TKey, typename TData>
-void adicionarNaHashTable(HashTable<TKey, TData> *tabela, TKey chave, TData valor);
+void destroiHashTable(HashTable<TKey, TData> *tabela)
+{
+    free(tabela);
+}
+
 template <typename TKey, typename TData>
-void removerDaHashTable(HashTable<TKey, TData> *tabela, TKey chave);
+void removerDaHashTable(HashTable<TKey, TData> *tabela, TKey chave) {}
 template <typename TKey, typename TData>
-Elemento<TKey, TData> *buscarNaHashTable(HashTable<TKey, TData> *tabela, TKey chave);
+Elemento<TKey, TData> *buscarNaHashTable(HashTable<TKey, TData> *tabela, TKey chave)
+{
+    Elemento<TKey, TData> *busca;
+
+    return busca;
+}
 
 #endif /* HASHTABLE_HPP */
--- ListaEncadeada.hpp
+++ ListaEncadeada.hpp
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include <exception>
 #include <new>
+#include <stdlib.h>
+#include <string>
 
 class posicao_invalida_exception : public std::exception
 {
@@ -28,40 +30,206 @@ struct ListaEncadeada
     Elemento<TKey, TData> *_primeiro;
     int _quantidade;
 };
-template <typename TKey, typename TData>
-ListaEncadeada<TKey, TData> *iniciaListaEncadeada();
 
 template <typename TKey, typename TData>
-void destroiListaEncadeada(ListaEncadeada<TKey, TData> *umaLista);
+ListaEncadeada<TKey, TData> *iniciaListaEncadeada()
+{
+    ListaEncadeada<TKey, TData> *lista = (ListaEncadeada<TKey, TData> *)calloc(1, sizeof(ListaEncadeada<TKey, TData>));
+    return lista;
+}
+template <typename TKey, typename TData>
+void destroiListaEncadeada(ListaEncadeada<TKey, TData> *umaLista)
+{
+    Elemento<TKey, TData> *atual = nullptr;
+    Elemento<TKey, TData> *proximo = nullptr;
+
+    if (!listaVazia(umaLista))
+    {
+        atual = umaLista->_primeiro;
+        proximo = atual->_proximo;
+        free(atual);
+    }
+
+    while (proximo != nullptr)
+    {
+        atual = proximo;
+        proximo = atual->_proximo;
+        free(atual);
+    }
+
+    free(umaLista);
+}
+template <typename TKey, typename TData>
+bool listaVazia(ListaEncadeada<TKey, TData> *umaLista)
+{
+    if (umaLista != nullptr && umaLista->_quantidade != 0)
+    {
+        return false;
+    }
+    else
+        return true;
+}
+template <typename TKey, typename TData>
+void adicionaNoInicio(ListaEncadeada<TKey, TData> *umaLista, TData *umDado)
+{
+    Elemento<TKey, TData> *novo = (Elemento<TKey, TData> *)calloc(1, sizeof(Elemento<TKey, TData>));
+
+    novo->_proximo = umaLista->_primeiro;
+    novo->_dado = umDado;
+
+    umaLista->_primeiro = novo;
+    umaLista->_quantidade++;
+}
 
 template <typename TKey, typename TData>
-bool listaVazia(ListaEncadeada<TKey, TData> *umaLista);
+Elemento<TKey, TData> *getN(ListaEncadeada<TKey, TData> *umaLista, int posicao)
+{
+    Elemento<TKey, TData> *ret = umaLista->_primeiro;
 
+    for (int i = 0; i < posicao - 1; i++)
+    {
+        ret = ret->_proximo;
+    }
+
+    return ret;
+}
 template <typename TKey, typename TData>
-int posicao(ListaEncadeada<TKey, TData> *umaLista, void *umDado);
+void adicionaNaPosicao(ListaEncadeada<TKey, TData> *umaLista, TData *umDado, int umaPosicao)
+{
 
+    //EXCECAO
+    if (umaPosicao < 1 || umaPosicao > umaLista->_quantidade + 1)
+    {
+        throw posicao_invalida_exception();
+    }
+
+    if (umaPosicao == 1)
+    {
+        adicionaNoInicio(umaLista, umDado);
+    }
+    else
+    {
+        Elemento<TKey, TData> *novo = (Elemento<TKey, TData> *)calloc(1, sizeof(Elemento<TKey, TData>));
+        Elemento<TKey, TData> *anterior = getN(umaLista, umaPosicao - 1);
+
+        novo->_proximo = anterior->_proximo;
+        novo->_dado = umDado;
+        anterior->_proximo = novo;
+
+        umaLista->_quantidade++;
+    }
+}
 template <typename TKey, typename TData>
-bool contem(ListaEncadeada<TKey, TData> *umaLista, void *umDado);
-
+void adicionaNoFim(ListaEncadeada<TKey, TData> *umaLista, TData *umDado)
+{
+    adicionaNaPosicao(umaLista, umDado, umaLista->_quantidade + 1);
+}
 template <typename TKey, typename TData>
-void adicionaNoInicio(ListaEncadeada<TKey, TData> *umaLista, void *umDado);
+int posicao(ListaEncadeada<TKey, TData> *umaLista, TData *umDado)
+{
 
+    if (listaVazia(umaLista))
+    {
+        return -1;
+    }
+
+    Elemento<TKey, TData> *elemento = umaLista->_primeiro;
+
+    if (elemento->_dado == umDado)
+    {
+        return 1;
+    }
+
+    for (int i = 2; i < umaLista->_quantidade + 1; i++)
+    {
+        elemento = elemento->_proximo;
+
+        if (elemento->_dado == umDado)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
 template <typename TKey, typename TData>
-void adicionaNaPosicao(ListaEncadeada<TKey, TData> *umaLista, void *umDado, int umaPosicao);
+bool contem(ListaEncadeada<TKey, TData> *umaLista, TData *umDado)
+{
+    if (listaVazia(umaLista))
+    {
+        return false;
+    }
+    else
+    {
+        Elemento<TKey, TData> *elemento = umaLista->_primeiro;
 
+        for (int i = 1; i <= umaLista->_quantidade; i++)
+        {
+            if (elemento->_dado == umDado)
+            {
+                return true;
+            }
+            else if (elemento->_proximo != nullptr)
+            {
+                elemento = elemento->_proximo;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
 template <typename TKey, typename TData>
-void adicionaNoFim(ListaEncadeada<TKey, TData> *umaLista, void *umDado);
+TData *retiraDoInicio(ListaEncadeada<TKey, TData> *umaLista)
+{
+    if (umaLista == nullptr || umaLista->_quantidade == 0)
+        throw lista_encadeada_vazia_exception();
 
+    Elemento<TKey, TData> *elemento;
+    TData *dado;
+
+    elemento = umaLista->_primeiro;
+    dado = elemento->_dado;
+    umaLista->_primeiro = elemento->_proximo;
+    umaLista->_quantidade--;
+
+    free(elemento);
+
+    return dado;
+}
 template <typename TKey, typename TData>
-void *retiraDoInicio(ListaEncadeada<TKey, TData> *umaLista);
+TData *retiraDaPosicao(ListaEncadeada<TKey, TData> *umaLista, int umaPosicao)
+{
+    if (umaLista == nullptr || umaLista->_quantidade == 0)
+    {
+        throw lista_encadeada_vazia_exception();
+    }
 
-template <typename TKey, typename TData>
-void *retiraDaPosicao(ListaEncadeada<TKey, TData> *umaLista, int umaPosicao);
+    if (umaPosicao < 1 || umaPosicao > umaLista->_quantidade)
+    {
+        throw posicao_invalida_exception();
+    }
 
-template <typename TKey, typename TData>
-void *retiraDoFim(ListaEncadeada<TKey, TData> *umaLista);
+    if (umaPosicao == 1)
+    {
+        return retiraDoInicio(umaLista);
+    }
+    else
+    {
+        Elemento<TKey, TData> *sai;
+        Elemento<TKey, TData> *anterior = getN(umaLista, umaPosicao - 1);
+        TData *dado;
 
-template <typename TKey, typename TData>
-void *retiraEspecifico(ListaEncadeada<TKey, TData> *umaLista, void *umDado);
+        sai = anterior->_proximo;
+        dado = sai->_dado;
+        anterior->_proximo = sai->_proximo;
 
+        free(sai);
+        umaLista->_quantidade--;
+        return dado;
+    }
+}
 #endif /* LISTAENCADEADA_HPP */
